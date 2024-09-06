2f512b42409f140aac7043c1599fa4144d2dd4db
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-06 16:54:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	let res = "";
+	let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for (let i = 0; i < l; i++) {
+		const str = Math.floor(Math.random() * characters.length);
+		res += characters[str];
+	} return res;
+	
 }
 
 // Do not change the code below.
